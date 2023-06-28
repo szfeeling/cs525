c06e173e8e8b1b97753d237b859514cd830f34f6
burningwings <burningwings@MacBook-Pro-2.local>
2023-06-28 15:15:23 +0800

    modify

--- src/test_assign1_1.c
+++ src/test_assign1_1.c
@@ -1,7 +1,22 @@
 
 #include <stdio.h>
-
+#include "storage_mgr.h"
 int main(){
-    printf("Hello World");
+
+    if(createPageFile("../data/database.txt") == RC_OK){
+        printf("success\n");
+    }
+    else{
+        printf("fail\n");
+    }
+
+    SM_FileHandle fHandle;
+    if(openPageFile("../data/database.txt", &fHandle) == RC_OK){
+        printf("success\n");
+    }
+    else{
+        printf("fail\n");
+    }
+
     return 0;
 }
